50b4b256f0448f2dcb8a64878b0775f0df15c4fc
Daniel <dansolnik@gmail.com>
2017-12-15 21:03:02 -0600

    configured for heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, '../public');
 
 var app = express();
