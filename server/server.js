1c3fd7cce52634563945f2f8f77ab3ca5d6dc6e1
Daniel <dansolnik@gmail.com>
2017-12-16 12:32:55 -0600

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -20,13 +20,10 @@ io.on('connection', (socket) => {
     console.log('New User Connected!');
 
     socket.on('createMessage', (message) => {
-        console.log(message);
+        io.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     });
-
-    socket.emit('newMessage', {
-        from: 'me',
-        text: 'cool',
-        createdAt: 1234
-    });
-
 });
